60fc061a980bf8a79ac9223ea4552f1ea1f4a70f
Jason McRainey <jmcraineydev@gmail.com>
2022-09-04 22:18:25 +0900

    Add initial build of swagger documentation

--- src/server.js
+++ src/server.js
@@ -4,9 +4,34 @@ const venuesRouter = require("../routes/venues");
 const groupsRouter = require("../routes/groups");
 const eventsRouter = require("../routes/events");
 
+//SWAGGER setup
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: "3.0.3",
+    info: {
+      title: "TomoKuru API",
+      version: "1.0.0",
+      description: "API used for the TomoKuru App",
+    },
+    servers: [
+      {
+        url: "http://localhost:3001",
+      },
+    ],
+  },
+  apis: ["../routes/*.js"],
+};
+
+const swaggerSpecs = swaggerJsDoc(swaggerOptions);
+
 const setupExpressServer = () => {
   const app = express();
   app.use(express.json());
+
+  app.use("/api_dev_docs", swaggerUI.serve, swaggerUI.setup(swaggerSpecs));
 
   app.use("/api/users", usersRouter);
   app.use("/api/venues", venuesRouter);
