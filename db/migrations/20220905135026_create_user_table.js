43e3b009e1d76dd1cf7c1d1e1c9310dc6247f110
Jason McRainey <jmcraineydev@gmail.com>
2022-09-05 23:05:18 +0900

    FEAT: add create user table migration file

---
+++ db/migrations/20220905135026_create_user_table.js
@@ -0,0 +1,25 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("users", function (table) {
+    table.string("id").primary();
+    table.string("email", 32).unique().notNullable().index();
+    table.string("firebase_id", 64).unique();
+    table.string("account_type", 20);
+    table.string("active_active", 20);
+    table.string("first_name", 32);
+    table.string("city_ward", 32);
+    table.string("prefecture", 32);
+    table.string("title", 64);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  knex.schema.dropTable("users");
+};
---
+++ routes/swagger.js
@@ -0,0 +1,43 @@
+/**
+ * components:
+ *  schemas:
+ *    users:
+ *      type: object
+ *      required:
+ *        - id
+ *        - email
+ *      properties:
+ *        id:
+ *          type: string
+ *          description: The auto-generated id of the user
+ *        email:
+ *          type: string
+ *          description: The user's email address
+ *        account_type:
+ *          type: number
+ *          description: Identifies the type of account 0 = User & 2 = Vendor
+ *        active_account:
+ *          type: number
+ *          description: Identifies if the account is active: 0 = inactive, 1 = active
+ *        first_name:
+ *          type: string
+ *          description: User's first name
+ *        city_ward:
+ *          type: string
+ *          description: Town of the user
+ *        prefecture:
+ *          type: string
+ *          description: Prefecture of the user
+ *        title:
+ *          type: string
+ *          description: Title of the user
+ *      example:
+ *        id: ngk389d
+ *        email: john(at)example.com
+ *        account_type: 0
+ *        active_account: 1
+ *        first_name: John
+ *        city_ward: Shinjuku
+ *        prefecture: Tokyo
+ *        title: FullStack Developer
+ */
