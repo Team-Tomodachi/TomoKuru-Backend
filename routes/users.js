044dc46dcb953e842cf5333b7f5b9e2adccd5cef
Jason McRainey <jmcraineydev@gmail.com>
2022-09-06 20:39:07 +0900

    FEAT: add get by email and post endpoints for users

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const db = require("../src/knex.js");
+const nanoID = require("nanoid");
 
 /**
  * @swagger
@@ -17,8 +19,60 @@ const router = express.Router();
  *                  $Ref: '#/components/schemas/users'
  *
  */
-router.get("/", (req, res) => {
-  res.send("this is the user end point").status(200);
+router.get("/", async (req, res) => {
+  try {
+    const users = await db("users").select("*").timeout(1500);
+    res.send(users).status(200);
+  } catch (err) {
+    res.send(err).status(404);
+  }
+});
+
+/**
+ * @swagger
+ * /api/users:
+ *    get:
+ *      summary: Returns a list of all of the users
+ *      responses:
+ *        '200':
+ *          description: The list of users
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: array
+ *                items:
+ *                  $Ref: '#/components/schemas/users'
+ *
+ */
+router.get("/:email", async (req, res) => {
+  const { email } = req.params;
+  try {
+    const user = await db("users")
+      .select(
+        "email",
+        "first_name",
+        "account_type",
+        "account_active",
+        "city_ward",
+        "prefecture",
+        "title"
+      )
+      .where("email", email)
+      .timeout(1500);
+    res.send(user).status(204);
+  } catch (err) {
+    res.send(err).status(404);
+  }
 });
 
 module.exports = router;
+
+// table.string("id").primary();
+// table.string("email", 32).unique().notNullable().index();
+// table.string("firebase_id", 64).unique();
+// table.string("account_type", 20);
+// table.string("active_active", 20);
+// table.string("first_name", 32);
+// table.string("city_ward", 32);
+// table.string("prefecture", 32);
+// table.string("title", 64);
