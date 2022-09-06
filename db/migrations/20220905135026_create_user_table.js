f9a60b19dae5045a08664d319d8955b498bcc3f8
Jason McRainey <jmcraineydev@gmail.com>
2022-09-06 23:47:37 +0900

    FEAT: change table id from nanoid to uuidv4)

--- db/migrations/20220905135026_create_user_table.js
+++ db/migrations/20220905135026_create_user_table.js
@@ -4,7 +4,7 @@
  */
 exports.up = function (knex) {
   return knex.schema.createTable("users", function (table) {
-    table.string("id").primary();
+    table.uuid("id").primary().defaultTo(knex.raw("gen_random_uuid()"));
     table.string("email", 32).unique().notNullable().index();
     table.string("firebase_id", 64).unique();
     table.string("account_type", 20);
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../src/knex.js");
-const nanoID = require("nanoid");
+
+/**
+ * @swagger
+ * tags:
+ *   name: Users
+ *   description: APIs for user data
+ */
 
 /**
  * @swagger
  * /api/users:
  *    get:
  *      summary: Returns a list of all of the users
+ *      tags: [Users]
  *      responses:
  *        '200':
  *          description: The list of users
@@ -30,18 +37,28 @@ router.get("/", async (req, res) => {
 
 /**
  * @swagger
- * /api/users:
- *    get:
- *      summary: Returns a list of all of the users
- *      responses:
- *        '200':
- *          description: The list of users
- *          content:
- *            application/json:
- *              schema:
- *                type: array
- *                items:
- *                  $Ref: '#/components/schemas/users'
+ * /api/users/:
+ *  get:
+ *    summary: Returns a user by email
+ *    tags: [Users]
+ *    parameters:
+ *      - in: path
+ *        name: email
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: The user email
+ *    responses:
+ *      '200':
+ *        description: Returns a single user
+ *        content:
+ *          application/json:
+ *            schema:
+ *             type: array
+ *             items:
+ *              $Ref: '#/components/schemas/users'
+ *      '404':
+ *       description: User not found
  *
  */
 router.get("/:email", async (req, res) => {
@@ -65,13 +82,64 @@ router.get("/:email", async (req, res) => {
   }
 });
 
+router.post("/", async (req, res) => {
+  const {
+    email,
+    first_name,
+    firebase_id,
+    account_type,
+    city_ward,
+    prefecture,
+    title,
+  } = req.body;
+  const newUser = {
+    account_active: "active",
+    email: email,
+    first_name: first_name,
+    firebase_id: firebase_id,
+    account_type: account_type,
+    city_ward: city_ward || "",
+    prefecture: prefecture || "",
+    title: title || "",
+  };
+  try {
+    await db("users").insert(newUser);
+    res.status(204).end();
+  } catch (err) {
+    res.send(err).status(400);
+  }
+});
+
+router.patch("/:email", async (req, res) => {
+  const { email } = req.params;
+  const edits = req.body;
+  try {
+    await db("users").where("email", email).update(edits);
+    res.status(204).end();
+  } catch (err) {
+    res.send(err).status(404);
+  }
+});
+
+router.patch("/:email/activation", async (req, res) => {
+  const { email } = req.params;
+  try {
+    await db("users")
+      .where("email", email)
+      .update({ account_active: "inactive" });
+    res.status(204).end();
+  } catch (err) {
+    res.send(err).status(404);
+  }
+});
+
 module.exports = router;
 
 // table.string("id").primary();
 // table.string("email", 32).unique().notNullable().index();
 // table.string("firebase_id", 64).unique();
 // table.string("account_type", 20);
-// table.string("active_active", 20);
+// table.string("account_active", 20);
 // table.string("first_name", 32);
 // table.string("city_ward", 32);
 // table.string("prefecture", 32);
