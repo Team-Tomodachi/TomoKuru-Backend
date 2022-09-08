cfe27ced20599f09d653819bdc28bc3358096e6b
Jason McRainey <jmcraineydev@gmail.com>
2022-09-09 00:09:10 +0900

    FEAT: add venue_type column

---
+++ db/migrations/20220908143901_add_venue_type_to_venues.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.alterTable("venues", function (table) {
+    table.string("venue_type");
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.alterTable("venues", function (table) {
+    table.dropColumn("venue_type");
+  });
+};
