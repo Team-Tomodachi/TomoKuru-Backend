e25448514b8a10d2ef2e491bb7c6bc0c5affcc6d
Jason McRainey <jmcraineydev@gmail.com>
2022-09-08 20:03:52 +0900

    FEAT: add endpoint to return a list of all venues

--- routes/venues.js
+++ routes/venues.js
@@ -1,8 +1,37 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("this is the venues end point").status(200);
+/**
+ * @swagger
+ * tags:
+ *   name: Venues
+ *   description: APIs for venue data
+ */
+
+/**
+ * @swagger
+ * /api/venues:
+ *    get:
+ *      summary: Returns a list of all of the venues
+ *      tags: [Venues]
+ *      responses:
+ *        '200':
+ *          description: The list of venues
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: array
+ *                items:
+ *                  $Ref: '#/components/schemas/venues'
+ *
+ */
+router.get("/", async (req, res) => {
+  try {
+    const venues = await db("venues").select("*").timeout(1500);
+    res.send(venues).status(200);
+  } catch (err) {
+    res.send(err).status(404);
+  }
 });
 
 module.exports = router;
