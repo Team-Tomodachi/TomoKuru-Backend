85e37f118466c549ff6a127f584caf1935c39e28
Jason McRainey <jmcraineydev@gmail.com>
2022-09-15 22:43:39 +0900

    FEAT: add migration to set nullable column in events table

---
+++ db/migrations/20220915133915_update_events_table_venue_id_nullable.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.alterTable("events", function (table) {
+    table.setNullable("venue_id");
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.alterTable("events", function (table) {
+    table.dropNullable("venue_id");
+  });
+};
